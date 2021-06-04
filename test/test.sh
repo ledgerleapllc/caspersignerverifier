f5990b67889c77f8fbc0bd08c25a8a1fc6c6c3dd
= <=>
2021-06-04 13:10:06 -0400

    test script update

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
 python3 -m pip install ed25519 && \
 python3 -m pip install ecdsa && \
-python3 $SCRIPT_DIR/../sign.py 'hello' test.secret.key test.public.key && \
-php test.php && \
-python3 $SCRIPT_DIR/../sign.py -s 'hello' secp256k1.secret.key secp256k1.public.key && \
-php test.php
+python3 $SCRIPT_DIR/../sign.py 'hello' $SCRIPT_DIR/test.secret.key $SCRIPT_DIR/test.public.key && \
+php $SCRIPT_DIR/test.php && \
+python3 $SCRIPT_DIR/../sign.py -s 'hello' $SCRIPT_DIR/secp256k1.secret.key $SCRIPT_DIR/secp256k1.public.key && \
+php $SCRIPT_DIR/test.php
